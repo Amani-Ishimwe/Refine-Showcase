7626ce406915a66708e29d1297a7a61be1594849
Amani Ishimwe <amaniishimwe36@gmail.com>
2026-03-09 12:14:28 +0200

    fixed hero

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -13,7 +13,7 @@ const Hero: React.FC = () => {
           Designing digital products with <span className="font-serif italic text-muted">intentionality.</span>
         </h1>
 
-        <p className="text-sm md:text-base text-muted max-w-105">
+        <p className="te5xt-sm md:text-base text-muted max-w-105">
           I bridge the gap between design thinking and software engineering to build high-impact systems.
         </p>
 
